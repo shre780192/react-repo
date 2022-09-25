7bbdaf3904bf6b115c6c3aea914de48b09e511df
Shr Muk <shreya.mukund1325@gmail.com>
2022-09-25 22:46:12 +0530

    v1

---
+++ src/Components/Dashboard.js
@@ -0,0 +1,10 @@
+import React from "react";
+const Dashboard = () => {
+  return (
+    <div className="Dashboard">
+      <p>Dashboard!! Hurray!!</p>
+      {/* First tab content will go here */}
+    </div>
+  );
+};
+export default Dashboard;
